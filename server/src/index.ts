d5c9317a3dcbee96f0165fbcf32d3f86b79b1afb
Camille Mizony <cmizony@mizony.net>
2025-08-26 19:36:35 -0700

    [Server] Create userProfile API service and connect to Firestore Database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,47 @@
 import * as functions from "firebase-functions";
-import * as express from "express";
-import * as admin from "firebase-admin";
+import express from "express";
+import cors from "cors";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
-admin.initializeApp();
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCUFDNsISbSTt0hhxIGxtmOvROBbsbAGyk",
+  authDomain: "yelp-like-griterr.firebaseapp.com",
+  projectId: "yelp-like-griterr",
+  storageBucket: "yelp-like-griterr.firebasestorage.app",
+  messagingSenderId: "261614060546",
+  appId: "1:261614060546:web:0247abf7675e011660531b",
+});
 
 const app = express();
+app.use(cors());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello from Express on Firebase!");
+type UserProfile = {
+  name: string;
+  handle: string;
+  avatarUrl: string;
+  verified: boolean;
+  followers: string;
+  following: string;
+  postsCount: string;
+  bio: string;
+};
+
+app.get("/userProfile", async (req, res) => {
+  try {
+    const database = getFirestore(firebaseApp);
+    const usersRef = doc(database, "users", "FfJ6GmPUfwmWa9inObRw");
+    const userSnapshot = await getDoc(usersRef);
+
+    const user = userSnapshot.exists()
+      ? (userSnapshot.data() as UserProfile)
+      : null;
+
+    res.json(user);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).send("Error fetching user");
+  }
 });
 
 export const api = functions.https.onRequest(app);
